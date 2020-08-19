5f1802f96a35c0bb5248fcd6e2a56eec4017f186
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-08-19 19:31:27 -0400

    form validation

---
+++ subscribe.php
@@ -0,0 +1,41 @@
+
+<?php
+if (!empty($email)) {
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "root";
+    $dbname = "sonix";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From register Where email = ? Limit 1";
+     $INSERT = "INSERT Into subscribers (email) values(?)";
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("s", $email);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
+
+
+ 
